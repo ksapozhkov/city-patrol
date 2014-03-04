8fd57ddba7cf82ed6ff481e56f9d638aab169935
ksapozhkov <kirill.sap@gmail.com>
2014-03-04 19:39:59 +0900

    Init project

---
+++ CityPatrolTest/src/com/example/citypatroltest/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.citypatroltest;
+
+import com.example.citypatroltest.LocationScanner.ScanLocation;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements ScanLocation {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		LocationScanner scanner = new LocationScanner(this);
+		scanner.scan();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onScanFinished(final Location[] scannedData) {
+		runOnUiThread(new Runnable() {
+
+			@Override
+			public void run() {
+				Toast.makeText(MainActivity.this,
+						"Finished: " + scannedData.length, Toast.LENGTH_SHORT)
+						.show();
+
+			}
+		});
+
+	}
+
+}
